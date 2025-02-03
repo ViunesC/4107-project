1ef0bfc54d0a3bb0efc3ee5c85f3c7ddbc839b8c
ViunesC <mrfusking233@gmail.com>
2025-02-03 12:41:18 -0500

    create documents for indexing module

---
+++ a1/src/indexing.py
@@ -0,0 +1,39 @@
+"""
+Module for indexing tokenized documents
+:author JunYang Wang
+
+For CSI-4107 Assignment 1
+"""
+import json
+
+
+def get_index(documents) -> dict:
+    """
+    Generate inverted index from given documents
+
+    :param documents: tokenized documents
+    :return: a dictionary containing inverted index
+    """
+    inverted_index = {}
+    # TODO
+    return inverted_index
+
+
+def save_to_file(index):
+    """
+    Save index to JSON file
+
+    :param index: a dictionary containing inverted index
+    :return: text file containing index
+    """
+    # TODO
+
+
+def load_from_file(filename):
+    """
+    Load index from JSON file
+
+    :param filename: name of JSON file containing index
+    :return: a dictionary containing inverted index
+    """
+    # TODO
