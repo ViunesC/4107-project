4e8fe33065dd91249448dbc8bdf40d0d08730ac4
ViunesC <mrfusking233@gmail.com>
2025-02-04 10:25:02 -0500

    reformat code

--- a1/src/preprocessing.py
+++ a1/src/preprocessing.py
@@ -1,26 +1,27 @@
-
 import nltk
 import json
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
+
 nltk.download('punkt_tab')
 nltk.download('stopwords', quiet=True)
 
 stop_words = set(stopwords.words('english'))
 stemmer = PorterStemmer()
 
+
 def tonkenize(doc):
-    
     return word_tokenize(doc.lower())
 
+
 def remove_stopwords(doc):
-    
     return [token for token in doc if token not in stop_words]
 
+
 def stem_tokens(doc):
-    
     return [stemmer.stem(token) for token in doc]
+
 
 def preprocess_doc(doc):
     doc = tonkenize(doc)
@@ -28,8 +29,8 @@ def preprocess_doc(doc):
     doc = stem_tokens(doc)
     return doc
 
+
 def preprocess_documents(documents):
-    
     preprocessed_docs = []
     with open("preprocessed.jsonl", "w", encoding="utf-8") as out_file:
         for doc in documents:
@@ -41,14 +42,13 @@ def preprocess_documents(documents):
             text_tokens = preprocess_doc(body_text)
 
             new_doc = {
-                    "_id": id,
-                    "title": title_tokens,
-                    "text": text_tokens
-                }
+                "_id": id,
+                "title": title_tokens,
+                "text": text_tokens
+            }
             preprocessed_docs.append(new_doc)
-    
+
             out_file.write(json.dumps(new_doc, ensure_ascii=False))
             out_file.write("\n")
-    
-    return preprocessed_docs
 
+    return preprocessed_docs
