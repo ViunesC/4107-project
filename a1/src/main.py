a2270c2613eb1e7e1ff7b01a7575ef3dcb22a43e
Your Name <you@example.com>
2025-02-04 07:01:45 -0500

    step 1 update 1

--- a1/src/main.py
+++ a1/src/main.py
@@ -1,1 +1,17 @@
-print("test")
+from preprocessing import *
+import json
+
+print("Step 1 starts")
+doc_path ='./scifact/corpus.jsonl'
+
+#result file
+documents = []
+
+#load file
+with open(doc_path, "r", encoding="utf-8") as f:
+    for line in f:
+        doc_json = json.loads(line)
+        documents.append(doc_json)
+
+documents = preprocess_documents(documents)
+print("Step 1 ends")
