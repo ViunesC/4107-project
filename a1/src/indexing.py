a285739eea86cea34c571eb1581dd3d4bad5a135
ViunesC <mrfusking233@gmail.com>
2025-02-04 11:26:52 -0500

    update build_index(), save_index_tofile() and load_index_fromfile()

--- a1/src/indexing.py
+++ a1/src/indexing.py
@@ -15,25 +15,45 @@ def build_index(documents) -> dict:
     :return: a dictionary containing inverted index
     """
     inverted_index = {}
-    # TODO
+    for document in documents:
+        document_id = document["_id"]
+        content_tokens = document["text"]
+
+        for token in content_tokens:
+            # If the token does not appear before
+            if token not in inverted_index:
+                inverted_index[token] = {}
+
+            # If token is not discovered in current document
+            if document_id not in inverted_index[token]:
+                inverted_index[token][document_id] = 0
+
+            # Increment the token count
+            inverted_index[token][document] += 1
+
     return inverted_index
 
 
-def save_index_tofile(index):
+def save_index_tofile(index, file_name):
     """
     Save index to JSON file
 
     :param index: a dictionary containing inverted index
-    :return: text file containing index
+    :param file_name: name of the saved file
+    :return: JSON file containing index
     """
-    # TODO
+    with open(file_name, 'w', encoding='utf-8') as file:
+        json.dump(index, file, indent=4)
 
 
-def load_index_fromfile(filename):
+def load_index_fromfile(file_name) -> dict:
     """
     Load index from JSON file
 
-    :param filename: name of JSON file containing index
+    :param file_name: name of JSON file containing index
     :return: a dictionary containing inverted index
     """
-    # TODO
+    with open(file_name, 'r', encoding='utf-8') as file:
+        inverted_index = json.load(file)
+
+    return inverted_index
