9b3d54f8ce5cf7eeb83971be2a0ad7bc5d40532d
ViunesC <mrfusking233@gmail.com>
2025-02-04 23:07:41 -0500

    update processed dataset

--- a1/src/main.py
+++ a1/src/main.py
@@ -3,23 +3,23 @@ from indexing import *
 import json
 import time
 
-# print("Preprocessing documents...")
-# start = time.time()
-#
-# doc_path = '../scifact/corpus.jsonl'
-#
-# # result file
-# documents = []
-#
-# # load file
-# with open(doc_path, "r", encoding="utf-8") as f:
-#     for line in f:
-#         doc_json = json.loads(line)
-#         documents.append(doc_json)
-#
-# documents = preprocess_documents(documents)
-# end = time.time()
-# print(f"Preprocessing completed. Time elapsed: {end - start} seconds.")
+print("Preprocessing documents...")
+start = time.time()
+
+doc_path = '../scifact/corpus.jsonl'
+
+# result file
+documents = []
+
+# load file
+with open(doc_path, "r", encoding="utf-8") as f:
+    for line in f:
+        doc_json = json.loads(line)
+        documents.append(doc_json)
+
+documents = preprocess_documents(documents)
+end = time.time()
+print(f"Preprocessing completed. Time elapsed: {end - start} seconds.")
 
 
 # print("Indexing tokenized documents...")
